6c479592076f1668d8c16af93821517b5f28a470
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2026-02-23 07:13:23 +0100

    Refactor using record

--- prototype.java
+++ prototype.java
@@ -9,13 +9,10 @@ void main() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException,
     var provider = kg.generateKeyPair();
     var wallet = kg.generateKeyPair();
 
-    var salt = new byte[32];
-    var index = 1;
-    var info = new byte[0];
-
     var ka = KeyAgreement.getInstance("ECDH");
-    var secretA = exchange(ka, provider.getPrivate(), hdk(wallet.getPublic(), salt, index, info)).generateSecret();
-    var secretB = exchange(ka, wallet.getPrivate(), hdk(provider.getPublic(), salt, index, info)).generateSecret();
+    var params = new HierarchicalDeterministicKeys(new byte[32], 1, new byte[0]);
+    var secretA = exchange(ka, provider.getPrivate(), params.derive(wallet.getPublic())).generateSecret();
+    var secretB = exchange(ka, wallet.getPrivate(), params.derive(provider.getPublic())).generateSecret();
 
     if (!MessageDigest.isEqual(secretA, secretB)) throw new AssertionError();
 
@@ -28,50 +25,58 @@ static KeyAgreement exchange(KeyAgreement ka, PrivateKey sk, PublicKey pk) throw
     return ka;
 }
 
-/**
- * Hierarchical deterministic keys.
- *
- * Assumes the input key is on the P-256 curve.
- *
- * Returns (x, y) or (x, p-y). This is OK for wallet applications.
- */
-ECPublicKey hdk(PublicKey key, byte[] salt, int index, byte[] info) throws NoSuchAlgorithmException {
-    final var TAG = "HDK-v1.";
-    final var SALT_SIZE = 32;
+record HierarchicalDeterministicKeys(byte[] salt, int index, byte[] info) {
+    private static final int SALT_SIZE = 32;
+    private static final String TAG = "HDK-v1.";
 
-    if (salt.length != SALT_SIZE) throw new IllegalArgumentException();
-    if (!(key instanceof ECPublicKey)) throw new IllegalArgumentException();
+    /**
+     * Assumes the input key is on the P-256 curve.
+     *
+     * Returns (x, y) or (x, p-y). This is OK for wallet applications.
+     */
+    ECPublicKey derive(PublicKey key) throws NoSuchAlgorithmException {
+        if (salt.length != SALT_SIZE) throw new IllegalArgumentException();
+        if (!(key instanceof ECPublicKey)) throw new IllegalArgumentException();
 
-    var digest = MessageDigest.getInstance("SHA-256");
-    var factory = KeyFactory.getInstance("EC");
-    var agreement = KeyAgreement.getInstance("ECDH");
+        var digest = MessageDigest.getInstance("SHA-256");
+        var factory = KeyFactory.getInstance("EC");
+        var agreement = KeyAgreement.getInstance("ECDH");
 
-    var p256 = ((ECPublicKey) key).getParams();
-    var prime = ((ECFieldFp) p256.getCurve().getField()).getP();
+        var p256 = ((ECPublicKey) key).getParams();
+        var factor = factor(digest, p256);
+        try {
+            exchange(agreement, factory.generatePrivate(new ECPrivateKeySpec(factor, p256)), key);
+        } catch (KeyException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
 
-    digest.reset();
-    digest.update(TAG.getBytes(StandardCharsets.US_ASCII));
-    digest.update(salt);
-    digest.update(ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(index).array());
-    var factor = new BigInteger(1, digest.digest(info))
-            .mod(p256.getOrder().subtract(BigInteger.ONE))
-            .add(BigInteger.ONE);
-    try {
-        exchange(agreement, factory.generatePrivate(new ECPrivateKeySpec(factor, p256)), key);
-    } catch (KeyException | InvalidKeySpecException e) {
-        throw new RuntimeException(e);
+        var sharedSecret = agreement.generateSecret();
+        return recover(factory, p256, sharedSecret);
     }
 
-    var x = new BigInteger(1, agreement.generateSecret()).mod(prime);
-    var y = x.modPow(BigInteger.valueOf(3), prime)
-            .add(p256.getCurve().getA().multiply(x))
-            .add(p256.getCurve().getB())
-            .mod(prime)
-            .modPow(prime.add(BigInteger.ONE).shiftRight(2), prime);
-    var point = new ECPoint(x, (y.testBit(0)) ? prime.subtract(y) : y);
-    try {
-        return (ECPublicKey) factory.generatePublic(new ECPublicKeySpec(point, p256));
-    } catch (InvalidKeySpecException e) {
-        throw new RuntimeException(e);
+    private BigInteger factor(MessageDigest digest, ECParameterSpec p256) {
+        digest.reset();
+        digest.update(TAG.getBytes(StandardCharsets.US_ASCII));
+        digest.update(salt);
+        digest.update(ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(index).array());
+        return new BigInteger(1, digest.digest(info))
+                .mod(p256.getOrder().subtract(BigInteger.ONE))
+                .add(BigInteger.ONE);
+    }
+
+    private ECPublicKey recover(KeyFactory factory, ECParameterSpec p256, byte[] sharedSecret) {
+        var prime = ((ECFieldFp) p256.getCurve().getField()).getP();
+        var x = new BigInteger(1, sharedSecret).mod(prime);
+        var y = x.modPow(BigInteger.valueOf(3), prime)
+                .add(p256.getCurve().getA().multiply(x))
+                .add(p256.getCurve().getB())
+                .mod(prime)
+                .modPow(prime.add(BigInteger.ONE).shiftRight(2), prime);
+        var point = new ECPoint(x, (y.testBit(0)) ? prime.subtract(y) : y);
+        try {
+            return (ECPublicKey) factory.generatePublic(new ECPublicKeySpec(point, p256));
+        } catch (InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
